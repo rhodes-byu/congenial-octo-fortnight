24656169b1b78e0f8d46f4e458e086bba08492ac
Jake Rhodes <jakerhodes8@gmail.com>
2024-10-31 10:25:41 -0600

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'fortnight',
-    version = '0.0.1',
+    version = '0.0.2',
     packages = find_packages(),
     install_requires = ['numpy', 'pandas']
 )
