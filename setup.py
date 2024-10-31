df3547f4f68eb113b37de1e2b9e81604a9c9306c
Jake Rhodes <jakerhodes8@gmail.com>
2024-10-31 10:08:10 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+# with open('requirements.txt') as f:
+#     requirements = f.readlines()
+
+
+setup(
+    name = 'fortnight',
+    version = '0.0.1',
+    packages = find_packages(),
+    requires = ['numpy', 'pandas']
+)
